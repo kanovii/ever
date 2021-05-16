76b07e963b5848246086ab7bcb699bd4d7e0b53d
kanovii <kkh0510ks@daum.net>
2021-05-16 18:35:43 +0900

    img js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 
 
-    var value = true;
 
-
+/*
 
     if(value === true) {
         document.getElementById('bg_img1').addEventListener('click', imgClk);
@@ -28,29 +27,56 @@
 
     
     }
-
+*/
 
 function imgClk() {
 
-    this.style.width = '100vw';
-    this.style.height = '100vh';
-    this.style.zIndex = '50';
-    this.style.position = 'absolute';
-    this.style.backgroundPosition = 'center';
-    this.style.backgroundSize = 'contain';
-    this.style.backgroundRepeat = 'no-repeat';
+    if(valuee == 1){
+        this.style.width = '100vw';
+        this.style.height = '100vh';
+        this.style.zIndex = '50';
+        this.style.position = 'absolute';
+        this.style.backgroundPosition = 'center';
+        this.style.backgroundSize = 'contain';
+        this.style.backgroundRepeat = 'no-repeat';
+        this.style.top = '-150px'    
+        /*
+        x.style.visibility = 'visible '; 
+        x.style.position = 'absolute';  
+        */ 
+        valuee--;
 
-    
+    }else {
+        
+        this.style.backgroundSize = 'cover';   
+
+        this.style.width = '320px';
+        this.style.height = this.height;
+        this.style.top = '0px'; 
+        this.style.position = 'relative';
+        this.style.zIndex = this.zIndex;
+        
+
+
+        this.style.backgroundPosition = 'center';
+        this.style.backgroundRepeat = this.backgroundRepeat;
+
+        valuee = 1;
+        
+    }
 }
-function imgClk2() {
 
-    this.style.width = '100vw';
-    this.style.height = '100vh';
-    this.style.zIndex = '50';
-    this.style.position = 'absolute';
-    this.style.backgroundPosition = 'center';
-    this.style.backgroundSize = 'contain';
-    this.style.backgroundRepeat = 'no-repeat';
 
-    
-}
+
+
+var valuee = 1;
+
+
+document.getElementById('bg_img1').addEventListener('click', imgClk);
+document.getElementById('bg_img2').addEventListener('click', imgClk);
+document.getElementById('bg_img3').addEventListener('click', imgClk);
+document.getElementById('bg_img4').addEventListener('click', imgClk);
+document.getElementById('bg_img5').addEventListener('click', imgClk);
+document.getElementById('bg_img6').addEventListener('click', imgClk);
+document.getElementById('bg_img7').addEventListener('click', imgClk);
+
