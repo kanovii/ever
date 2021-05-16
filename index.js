9bd6c6ae5d97130fac7499b37071f25a9cb87720
kanovii <kkh0510ks@daum.net>
2021-05-16 18:55:46 +0900

    dek

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 
 
-
-
-/*
-
-    if(value === true) {
-        document.getElementById('bg_img1').addEventListener('click', imgClk);
-        document.getElementById('bg_img2').addEventListener('click', imgClk);
-        document.getElementById('bg_img3').addEventListener('click', imgClk);
-        document.getElementById('bg_img4').addEventListener('click', imgClk);
-        document.getElementById('bg_img5').addEventListener('click', imgClk);
-        document.getElementById('bg_img6').addEventListener('click', imgClk);
-        document.getElementById('bg_img7').addEventListener('click', imgClk);
-
-
-        value = false;
-        } else {
-            document.getElementById('bg_img1').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img2').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img3').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img4').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img5').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img6').removeEventListener('click', arguments.callee);
-            document.getElementById('bg_img7').removeEventListener('click', arguments.callee);
-
-
-    
-    }
-*/
-
 function imgClk() {
 
     if(valuee == 1){
@@ -40,10 +11,7 @@ function imgClk() {
         this.style.backgroundSize = 'contain';
         this.style.backgroundRepeat = 'no-repeat';
         this.style.top = '-150px';    
-        /*
-        x.style.visibility = 'visible '; 
-        x.style.position = 'absolute';  
-        */ 
+        
         valuee--;
 
     }else {
