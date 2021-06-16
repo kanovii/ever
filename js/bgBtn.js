defa2f81a1d52c94bf1af4a9a15c7291d304fa32
kanovii <kkh0510ks@daum.net>
2021-06-16 15:22:00 +0900

    header color

--- js/bgBtn.js
+++ js/bgBtn.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('bg_btn');
 const bx = document.getElementsByClassName('box');
 const bd = document.getElementsByClassName('bar');
+const header = document.getElementsByClassName('header');
 
 
 btn.addEventListener('click', change);
@@ -10,11 +11,15 @@ function change() {
     
     if (x == 1) {
         bx[0].style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
+        header[0].style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
+
       //  bd[0].style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
         x++
     }
     else {
         bx[0].style.backgroundColor = 'white';
+        header[0].style.backgroundColor = 'white';
+
      //   bd[0].style.backgroundColor = 'white';
         x = 1;
     }
