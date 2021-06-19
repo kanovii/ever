7d25e229012b57cb6a567ba632f6370ce3e4f6f7
kanovii <kkh0510ks@daum.net>
2021-06-19 22:51:01 +0900

    bg color

--- js/bgBtn.js
+++ js/bgBtn.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('bg_btn');
 const bx = document.getElementsByClassName('box');
 const bd = document.getElementsByClassName('bar');
 const header = document.getElementsByClassName('header');
-
+const bod = document.getElementsByTagName('body');
 
 btn.addEventListener('click', change);
 let x = 1;
@@ -10,14 +10,18 @@ function change() {
 
     
     if (x == 1) {
-        boxBox.style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
-        header[0].style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
+        boxBox.style.backgroundColor = 'rgba(121, 51, 161, 0.1)';
+        header[0].style.backgroundColor = 'rgba(121, 51, 161, 0.1)';
+        bod[0].style.backgroundColor = 'rgba(121, 51, 161, 0.1)';
+
         
       //  bd[0].style.backgroundColor = 'rgba(121, 51, 161, 0.2)';
         x++;
     } else {
         boxBox.style.backgroundColor = 'white';
         header[0].style.backgroundColor = 'white';
+        bod[0].style.backgroundColor = 'white';
+
 
      //   bd[0].style.backgroundColor = 'white';
         x = 1;
