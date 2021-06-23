b24fe679de51bbbf2f7107f1241d8d831608cd88
kanovii <kkh0510ks@daum.net>
2021-06-23 15:31:23 +0900

    edit index js

--- js/index.js
+++ js/index.js
@@ -1,56 +1,57 @@
 
 
-function imgClk() {
+function imgClk(event) {
 
     if(valuee == 1){
         if(matchMedia("screen and (min-width: 768px)").matches){
-            this.style.width = '760px';
-            this.style.height = '700px';
-            this.style.top = '-140px';
+            event.target.style.width = '760px';
+            event.target.style.height = '700px';
+            event.target.style.top = '-140px';
+
+
 
         }else{
-            this.style.width = '90vw';
-            this.style.height = '60vh';
-            this.style.top = '0px';    
+            event.target.style.width = '90vw';
+            event.target.style.height = '60vh';
+            event.target.style.top = '0';
 
         }
-   
-        this.style.zIndex = '50';
-        this.style.position = 'absolute';
-        this.style.backgroundPosition = 'center';
-        this.style.backgroundSize = 'cover';
-        this.style.backgroundRepeat = 'no-repeat';
-        this.style.boxShadow = '1px 1px 5px 2px rgba(70, 23, 97, 0.3)';
+        event.target.style.zIndex = '50';
+        event.target.style.position = 'absolute';
+        event.target.style.backgroundPosition = 'center';
+        event.target.style.backgroundSize = 'cover';
+        event.target.style.backgroundRepeat = 'no-repeat';
+        event.target.style.boxShadow = '1px 1px 5px 2px rgba(70, 23, 97, 0.3)';
         
         valuee--;
 
     }else {
         
         if(matchMedia("screen and (min-width: 768px)").matches){
-            this.style.width = '520px';
-            this.style.height = '400px';
+            event.target.style.width = '520px';
+            event.target.style.height = '400px';
         }else{
-            this.style.width = '320px';
-            this.style.height = this.height;
+            event.target.style.width = '320px';
+            event.target.style.height = '400px';
         }
-        this.style.backgroundSize = 'cover';   
-        this.style.top = '0px'; 
-        this.style.position = 'relative';
-        this.style.zIndex = "49";
+        event.target.style.backgroundSize = 'cover';
+        event.target.style.top = '0';
+        event.target.style.position = 'relative';
+        event.target.style.zIndex = "49";
         
 
 
-        this.style.backgroundPosition = 'center';
-        this.style.backgroundRepeat = this.backgroundRepeat;
+        event.target.style.backgroundPosition = 'center';
+        event.target.style.backgroundRepeat = this.backgroundRepeat;
 
-        this.style.boxShadow = 'unset';
+        event.target.style.boxShadow = 'unset';
 
 
         valuee = 1;
         
     }
 }
-
+/*
 document.getElementById('bg_img1').addEventListener('click', imgClk);
 document.getElementById('bg_img2').addEventListener('click', imgClk);
 document.getElementById('bg_img3').addEventListener('click', imgClk);
@@ -65,11 +66,20 @@ document.getElementById('bg_img11').addEventListener('click', imgClk);
 document.getElementById('bg_img12').addEventListener('click', imgClk);
 document.getElementById('bg_img13').addEventListener('click', imgClk);
 document.getElementById('bg_img14').addEventListener('click', imgClk);
-document.getElementById('bg_img15').addEventListener('click', imgClk);
+document.getElementsByClassName('bg_img')[14].addEventListener('click', imgClk);
+*/
 
-
+const bgbg = document.querySelectorAll('.bg_img');
 var valuee = 1;
 
+function setNum () {
+    for(let i = 0; i < bgbg.length; i++) {
+        bgbg[i].addEventListener('click', imgClk);
 
+    }
+}
 
-
+function init () {
+    setNum();
+}
+init ();
